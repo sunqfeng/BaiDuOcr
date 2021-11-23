f2d7aeb97207a95f348d8f0b7a69cbb4189f631d
sunqfeng <18722358342@163.com>
2021-11-23 09:21:32 +0800

    add by sqf for 2021年11月23日09:21:29.

--- BaiduOcr/src/main/java/Org/BaiduOcr/GeneralBasic.java
+++ BaiduOcr/src/main/java/Org/BaiduOcr/GeneralBasic.java
@@ -23,69 +23,62 @@ public class GeneralBasic {
 	 * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
 	 * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3 下载
 	 */
-	public static String generalBasic(String filePath)
-	{
+	public static String generalBasic(String filePath) {
 		// 请求url
 		String url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic";
-		try
-		{
+		try {
 			// 本地文件路径
 //			String filePath = "D://java_picture//wocao.jpg";
 
 			byte[] imgData = FileUtil.readFileByBytes(filePath);
 			String imgStr = Base64Util.encode(imgData);
 			String imgParam = URLEncoder.encode(imgStr, "UTF-8");
- 
+
 			String param = "image=" + imgParam;
- 
+
 			// 注意这里仅为了简化编码每一次请求都去获取access_token，线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
 			AuthService authService = new AuthService();
 			String token = authService.getAuth();
 			System.out.println("token = " + token);
 			String accessToken = token;
- 
+
 			String result = HttpUtil.post(url, accessToken, param);
 //			System.out.println("sqf==== "+result);
 
-			String obj = result.substring(result.indexOf("[")+1,result.indexOf("]"));
-			System.out.println("obj==="+ obj );
+			String obj = result.substring(result.indexOf("[") + 1, result.indexOf("]"));
+			System.out.println("obj===" + obj);
 			String[] arry = obj.split("\\},");
-			for (int i = 0;i<arry.length;i++)
-			{
+			for (int i = 0; i < arry.length; i++) {
 				String[] arry1 = arry[i].split("\\:");
 				System.out.println(arry1[1]);
 //				System.out.println( arry1[1].replaceAll("\\s*","").replaceAll("[^(\\u4e00-\\u9fa5)]",""));
 			}
 
 			return result;
-		} 
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
- 
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 
 		System.out.println("已开始...");
 		String path = "D://java_picture//";
 
 //		List<String> files = new ArrayList<String>();
-        File file = new File(path);
-        File[] tempList = file.listFiles();
+		File file = new File(path);
+		File[] tempList = file.listFiles();
 
-        for (int i = 0; i < tempList.length; i++) {
-            if (tempList[i].isFile()) {
+		for (int i = 0; i < tempList.length; i++) {
+			if (tempList[i].isFile()) {
 //                files.add(tempList[i].toString());
-            	GeneralBasic.generalBasic( tempList[i].toString()  );
-            }
-            if (tempList[i].isDirectory()) {
-            }
-        }
+				GeneralBasic.generalBasic(tempList[i].toString());
+			}
+			if (tempList[i].isDirectory()) {
+			}
+		}
 
 	}
-
 
 }
